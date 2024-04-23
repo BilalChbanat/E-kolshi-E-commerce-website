721160d1463fec8be5e5a9ce713d0c57ad2d01c9
BilalChbanat <bilal.chbanat2003@gmail.com>
2024-04-23 15:05:52 +0100

    fixing navigations

--- public/assets/js/filters.js
+++ public/assets/js/filters.js
@@ -42,23 +42,22 @@ function displayProducts(products) {
             product.shortDescription = product.description;
         }
         articleContainer.innerHTML += `
-        <div class="p-8 bg-white rounded-lg shadow-lg">
-            <div class="relative overflow-hidden">
-                <img class="object-cover" src="http://127.0.0.1:8000/${product.image}" alt="Product" style="width: 300px; height: 250px;">
-                <div class="absolute inset-0 bg-black opacity-40"></div>
-                <div class="absolute inset-0 flex items-center justify-center">
-                    <button class="px-6 py-2 font-bold text-gray-900 bg-white rounded-full hover:bg-gray-300">View Product</button>
+            <div class="p-8 bg-white rounded-lg shadow-lg">
+                <div class="relative overflow-hidden">
+                    <img class="object-cover" src="http://127.0.0.1:8000/${product.image}" alt="Product" style="width: 300px; height: 250px;">
+                    <div class="absolute inset-0 bg-black opacity-40"></div>
+                    <div class="absolute inset-0 flex items-center justify-center">
+                        <a href="${product.id}/detail" class="px-6 py-2 font-bold text-gray-900 bg-white rounded-full hover:bg-gray-300">View Product</a>
+                    </div>
+                </div>
+
+                <h3 class="mt-4 text-xl font-bold text-gray-900">${product.title}</h3>
+                <p class="mt-2 text-sm text-gray-500 min-w-24 max-h-28 min-h-28">${product.shortDescription}</p>
+                <div class="flex items-center justify-between mt-4">
+                    <span class="text-lg font-bold text-gray-900">${product.price} MAD</span>
+                    <button class="px-4 py-2 font-bold text-white bg-primary-100 rounded-full hover:bg-primary-300">Add to Cart</button>
                 </div>
             </div>
-
-            <h3 class="mt-4 text-xl font-bold text-gray-900">${product.title}</h3>
-            <p class="mt-2 text-sm text-gray-500 min-w-24 max-h-28 min-h-28">${product.shortDescription}</p>
-            <div class="flex items-center justify-between mt-4">
-                <span class="text-lg font-bold text-gray-900">${product.price} MAD</span>
-                <button class="px-4 py-2 font-bold text-white bg-primary-100 rounded-full hover:bg-primary-300">Add to Cart</button>
-            </div>
-        </div>
         `;
     });
 }
-
--- resources/views/dashboard/products/show.blade.php
+++ resources/views/dashboard/products/show.blade.php
@@ -49,11 +49,15 @@
                         </svg>
                         <h4 class="text-[#333] text-base">500 Reviews</h4>
                     </div>
+                    <br>
+                    <span class="pt-16">
+                        {{ $product->description }}
+                    </span>
 
                     <div class="flex flex-wrap gap-4 mt-10">
-                        <button type="button"
-                            class="min-w-[200px] px-4 py-3 bg-[#333] hover:bg-[#111] text-white text-sm font-bold rounded">Buy
-                            now</button>
+                        <a href="{{ route('wishlist.add', $product->id) }}"
+                            class="min-w-[200px] px-4 py-3 bg-[#333] pl-12 hover:bg-[#111] text-white text-sm font-bold rounded">Add to WishList
+                            </a>
                         <a href="{{ route('cart.add', $product->id) }}"
                             class="min-w-[200px] pl-[3.3rem] py-2.5 border border-[#333] bg-transparent hover:bg-gray-50 text-[#333] text-sm font-bold rounded">Add
                             to cart</a>
@@ -76,117 +80,7 @@
 
                 </ul>
             </div>
-            <div class="mt-16 shadow-[0_2px_10px_-3px_rgba(6,81,237,0.3)] p-6">
-                <h3 class="text-lg font-bold text-[#333]">Reviews(10)</h3>
-                <div class="grid md:grid-cols-2 gap-12 mt-6">
-                    <div>
-                        <div class="space-y-3">
-                            <div class="flex items-center">
-                                <p class="text-sm text-[#333] font-bold">5.0</p>
-                                <svg class="w-5 fill-[#333] ml-1" viewBox="0 0 14 13" fill="none"
-                                    xmlns="http://www.w3.org/2000/svg">
-                                    <path
-                                        d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
-                                </svg>
-                                <div class="bg-gray-400 rounded w-full h-2 ml-3">
-                                    <div class="w-2/3 h-full rounded bg-[#333]"></div>
-                                </div>
-                                <p class="text-sm text-[#333] font-bold ml-3">66%</p>
-                            </div>
-                            <div class="flex items-center">
-                                <p class="text-sm text-[#333] font-bold">4.0</p>
-                                <svg class="w-5 fill-[#333] ml-1" viewBox="0 0 14 13" fill="none"
-                                    xmlns="http://www.w3.org/2000/svg">
-                                    <path
-                                        d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
-                                </svg>
-                                <div class="bg-gray-400 rounded w-full h-2 ml-3">
-                                    <div class="w-1/3 h-full rounded bg-[#333]"></div>
-                                </div>
-                                <p class="text-sm text-[#333] font-bold ml-3">33%</p>
-                            </div>
-                            <div class="flex items-center">
-                                <p class="text-sm text-[#333] font-bold">3.0</p>
-                                <svg class="w-5 fill-[#333] ml-1" viewBox="0 0 14 13" fill="none"
-                                    xmlns="http://www.w3.org/2000/svg">
-                                    <path
-                                        d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
-                                </svg>
-                                <div class="bg-gray-400 rounded w-full h-2 ml-3">
-                                    <div class="w-1/6 h-full rounded bg-[#333]"></div>
-                                </div>
-                                <p class="text-sm text-[#333] font-bold ml-3">16%</p>
-                            </div>
-                            <div class="flex items-center">
-                                <p class="text-sm text-[#333] font-bold">2.0</p>
-                                <svg class="w-5 fill-[#333] ml-1" viewBox="0 0 14 13" fill="none"
-                                    xmlns="http://www.w3.org/2000/svg">
-                                    <path
-                                        d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
-                                </svg>
-                                <div class="bg-gray-400 rounded w-full h-2 ml-3">
-                                    <div class="w-1/12 h-full rounded bg-[#333]"></div>
-                                </div>
-                                <p class="text-sm text-[#333] font-bold ml-3">8%</p>
-                            </div>
-                            <div class="flex items-center">
-                                <p class="text-sm text-[#333] font-bold">1.0</p>
-                                <svg class="w-5 fill-[#333] ml-1" viewBox="0 0 14 13" fill="none"
-                                    xmlns="http://www.w3.org/2000/svg">
-                                    <path
-                                        d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
-                                </svg>
-                                <div class="bg-gray-400 rounded w-full h-2 ml-3">
-                                    <div class="w-[6%] h-full rounded bg-[#333]"></div>
-                                </div>
-                                <p class="text-sm text-[#333] font-bold ml-3">6%</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="">
-                        <div class="flex items-start">
-                            <img src="https://readymadeui.com/team-2.webp"
-                                class="w-12 h-12 rounded-full border-2 border-white" />
-                            <div class="ml-3">
-                                <h4 class="text-sm font-bold text-[#333]">{{ $product->productSeller->name }}</h4>
-                                <div class="flex space-x-1 mt-1">
-                                    <svg class="w-4 fill-[#333]" viewBox="0 0 14 13" fill="none"
-                                        xmlns="http://www.w3.org/2000/svg">
-                                        <path
-                                            d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
-                                    </svg>
-                                    <svg class="w-4 fill-[#333]" viewBox="0 0 14 13" fill="none"
-                                        xmlns="http://www.w3.org/2000/svg">
-                                        <path
-                                            d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
-                                    </svg>
-                                    <svg class="w-4 fill-[#333]" viewBox="0 0 14 13" fill="none"
-                                        xmlns="http://www.w3.org/2000/svg">
-                                        <path
-                                            d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
-                                    </svg>
-                                    <svg class="w-4 fill-[#CED5D8]" viewBox="0 0 14 13" fill="none"
-                                        xmlns="http://www.w3.org/2000/svg">
-                                        <path
-                                            d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
-                                    </svg>
-                                    <svg class="w-4 fill-[#CED5D8]" viewBox="0 0 14 13" fill="none"
-                                        xmlns="http://www.w3.org/2000/svg">
-                                        <path
-                                            d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
-                                    </svg>
-                                    <p class="text-xs !ml-2 font-semibold text-[#333]">
-                                        {{ \Carbon\Carbon::parse($product->created_at)->diffForHumans() }}</p>
-                                </div>
-                                <p class="text-sm mt-4 text-[#333]">{{$product->description}}</p>
-                            </div>
-                        </div>
-                        <button type="button"
-                            class="w-full mt-10 px-4 py-2.5 bg-transparent hover:bg-gray-50 border border-[#333] text-[#333] font-bold rounded">Read
-                            all reviews</button>
-                    </div>
-                </div>
-            </div>
+            
         </div>
     </div>
 @endsection
